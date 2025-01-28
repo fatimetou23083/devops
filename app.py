714e0e594a0b5653b0a1b722439432ba51553cec
fatimetou23083 <23083@supnum.mr>
2025-01-28 17:13:25 +0000

    version2

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 # app.py
 from flask import Flask, jsonify
-import datetime
+# import datetime
+
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify({
-        "message": "Bienvenue sur l'API ",
-        "timestamp": datetime.datetime.now().isoformat()
-    })
+    return "hello sara"
+#     return jsonify({
+#         "message": "Bienvenue sur l'API ",
+#         "timestamp": datetime.datetime.now().isoformat()
+#     })
 
-@app.route('/health ')
-def health():
-    return jsonify({
-        " status ": " healthy ",
-        " version ": " 1.0.0 "
-    })
+# @app.route('/health ')
+# def health():
+#     return jsonify({
+#         " status ": " healthy ",
+#         " version ": " 1.0.0 "
+#     })
 
 if __name__ == '__main__':
+    app.logger.info("Démarrage du serveur...")
+
     app.run(host='0.0.0.0', port=5000)
